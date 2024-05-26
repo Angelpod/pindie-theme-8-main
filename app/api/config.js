c1dfb5e46d9b962ba1dfb6dcb8b0c17c55225aa1
Angelpod <149606603+Angelpod@users.noreply.github.com>
2024-05-26 17:37:54 +0300

    url upade

--- app/api/config.js
+++ app/api/config.js
@@ -1,5 +1,5 @@
 export const BASE_URL = 'https://mydomain.ru.nomoredomainswork.ru/api' 
-//lkz rjvvbnf
+//lkz rjvvbnf...
 export const endpoints = {
   games: `${BASE_URL}/games`,
   auth: `${BASE_URL}/auth/login`,
